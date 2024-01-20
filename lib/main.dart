910846e915344ceab8c6a3c0fcb3484e63b0c49d
Calvin Ren <calvin.qut@gmail.com>
2024-01-20 23:05:07 +1000

    add tab function

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,120 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const MaterialApp(
+      home: MyTabbedPage(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyTabbedPage extends StatefulWidget {
+  const MyTabbedPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _MyTabbedPageState createState() => _MyTabbedPageState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class _MyTabbedPageState extends State<MyTabbedPage> {
+  int _selectedIndex = 0;
+  final PageStorageBucket bucket = PageStorageBucket();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+      body: PageStorage(
+        bucket: bucket,
+        child: switch (_selectedIndex) {
+          0 => const Page1(),
+          1 => const Page2(),
+          int() => const Page1(),
+        },
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: (int index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Page1 extends StatelessWidget {
+  const Page1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      key: const PageStorageKey('page1'),
+      child: Center(
+        child: ColoredBox(
+          color: Colors.blueAccent,
+          child: Column(
+            children: [
+              for (int i = 0; i < 100; i++)
+                Text(
+                  'Home $i',
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Color.fromARGB(
+                      255,
+                      Random().nextInt(255),
+                      Random().nextInt(255),
+                      Random().nextInt(255),
+                    ),
+                  ),
+                ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+    );
+  }
+}
+
+class Page2 extends StatelessWidget {
+  const Page2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      key: const PageStorageKey('page2'),
+      child: Center(
+        child: ColoredBox(
+          color: Colors.blueAccent,
+          child: Column(
+            children: [
+              for (int i = 0; i < 100; i++)
+                Text(
+                  'Business$i',
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Color.fromARGB(
+                      255,
+                      Random().nextInt(255),
+                      Random().nextInt(255),
+                      Random().nextInt(255),
+                    ),
+                  ),
+                ),
+            ],
+          ),
+        ),
       ),
     );
   }
